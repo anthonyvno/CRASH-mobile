a03307b2a9450985c8e04e8ce471b31d355449ed
Anthony Van Noppen <anthony.vannoppen@reamdolmen.com>
2019-04-12 14:55:25 +0200

    fotos ongeval

--- app/src/main/java/com/example/europeesaanrijdingsformulier/report/ReportImageFragment.kt
+++ app/src/main/java/com/example/europeesaanrijdingsformulier/report/ReportImageFragment.kt
@@ -2,22 +2,23 @@ package com.example.europeesaanrijdingsformulier.report
 
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
+import android.support.annotation.RequiresApi
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.esafirm.imagepicker.features.ImagePicker
 import com.esafirm.imagepicker.model.Image
 
-import com.example.europeesaanrijdingsformulier.R
-import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_report_image.*
-import java.io.InputStream
+import android.view.Gravity
+import android.widget.GridLayout
+import com.example.europeesaanrijdingsformulier.R
+
 
 class ReportImageFragment : Fragment() {
 
@@ -37,13 +38,14 @@ class ReportImageFragment : Fragment() {
     override fun onStart() {
         super.onStart()
 
-        button_report_image_camera.setOnClickListener{
-            ImagePicker.create(this) // Activity or Fragment
+        button_report_image_camera.setOnClickListener {
+            ImagePicker.create(this)
+                .limit(4)
                 .start()
 
         }
 
-        button_report_image_confirm.setOnClickListener{
+        button_report_image_confirm.setOnClickListener {
             val fragment = ReportOverviewFragment()
             fragment.addObject(report)
             this.fragmentManager!!.beginTransaction()
@@ -59,20 +61,55 @@ class ReportImageFragment : Fragment() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
+            //val linearList = listOf(LinearLayout1, LinearLayout2, LinearLayout3, LinearLayout4)
             // Get a list of picked images
             images = ImagePicker.getImages(data)
+            gridloller.removeAllViews()
             // or get a single image only
-            image = ImagePicker.getFirstImageOrNull(data)
-            val gson = Gson()
-            println(gson.toJson(image))
-            val bm = BitmapFactory.decodeFile(image.path)
-            println(bm)
-           //val bitmap = MediaStore.Images.Media.getBitmap(activity!!.contentResolver,image.)
-            Glide.with(image_report_image_picture)
-                .load(image.getPath())
-                .into(image_report_image_picture);
+            if(images.size ==1){
+                gridloller.columnCount = 1
+            }else{
+                gridloller.columnCount = 2
+            }
+            gridloller.rowCount = Math.ceil((images.size.toDouble()/2)).toInt()
+
+            for ((counter, img) in images.withIndex()) {
+
+                //linearList[counter].removeAllViews()
+                val iv = ImageView(activity)
+                iv.scaleType=ImageView.ScaleType.FIT_XY
+                iv.foreground = resources.getDrawable(R.drawable.black_border)
+                //iv.adjustViewBounds = false
+                Glide.with(iv)
+                    .load(img.path)
+                    .into(iv)
+
+                val param = GridLayout.LayoutParams()
+                if(images.size == 1){
+                    param.height = gridloller.measuredHeight
+                    param.width = gridloller.measuredWidth
+                } else if(images.size == 2){
+                    param.height = (gridloller.measuredHeight/1.5).toInt()
+                    param.width = gridloller.measuredWidth/2
+                } else {
+                    param.height = gridloller.measuredHeight/2
+                    param.width = gridloller.measuredWidth/2
+                }
+
+
+                //param.setGravity(Gravity.CENTER)
+
+                iv.layoutParams = param
+                gridloller.addView(iv)
+
+                //println(linearList[counter].width)
+                //linearList[counter].addView(iv)
+
+            }
+
             //image_report_image_picture.setImageBitmap(bm)
         }
         super.onActivityResult(requestCode, resultCode, data)
