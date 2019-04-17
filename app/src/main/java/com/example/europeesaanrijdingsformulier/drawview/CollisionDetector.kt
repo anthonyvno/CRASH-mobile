81eb3cf613603dc9b1437c398b9060fe92f040c9
Sander Beazar <Sander.Beazar@realdolmen.com>
2019-04-17 08:53:22 +0200

    nieuwe klassen voor collision

---
+++ app/src/main/java/com/example/europeesaanrijdingsformulier/drawview/CollisionDetector.kt
@@ -0,0 +1,12 @@
+package com.example.europeesaanrijdingsformulier.drawview
+
+class CollisionDetector {
+
+    lateinit var  hitboxA : MyRectangle
+    lateinit var  hitboxB : MyRectangle
+
+    fun willCollide(hitboxA:MyRectangle,hitboxB:MyRectangle ){
+
+    }
+
+}
---
+++ app/src/main/java/com/example/europeesaanrijdingsformulier/drawview/CollisionManager.kt
@@ -0,0 +1,8 @@
+package com.example.europeesaanrijdingsformulier.drawview
+
+import android.graphics.Canvas
+
+class CollisionManager {
+    lateinit var canvas: Canvas
+    
+}
