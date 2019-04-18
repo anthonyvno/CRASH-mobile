35dad25036ea4e0f16262b9f2ba7db41c83314f3
Sander Beazar <Sander.Beazar@realdolmen.com>
2019-04-18 10:35:30 +0200

    UI test finished

--- app/src/main/java/com/example/europeesaanrijdingsformulier/drawview/SituationManager.kt
+++ app/src/main/java/com/example/europeesaanrijdingsformulier/drawview/SituationManager.kt
@@ -46,7 +46,7 @@ class SituationManager(
         canvas.drawBitmap(
             bitmapA,
             rectA.center.x - bitmapA.width / 2,
-            rectA.center.y - bitmapA.height / 2,
+            rectA.center.y,
             paint
         )
 
@@ -58,7 +58,7 @@ class SituationManager(
         canvas.drawBitmap(
             bitmapB,
             rectB.center.x - bitmapB.width / 2,
-            rectB.center.y - bitmapB.height / 2,
+            rectB.center.y ,
             paint
         )
     }
