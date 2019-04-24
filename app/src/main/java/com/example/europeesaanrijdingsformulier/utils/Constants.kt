6b9fff4078b985c855db111ecd7d818d385074ad
Sander Beazar <Sander.Beazar@realdolmen.com>
2019-04-24 11:00:53 +0200

    backend string

--- app/src/main/java/com/example/europeesaanrijdingsformulier/utils/Constants.kt
+++ app/src/main/java/com/example/europeesaanrijdingsformulier/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.anthonyvannoppen.androidproject.utils
 
-const val BASE_URL: String = "http://10.0.2.2:8080/"
+const val BASE_URL: String = "http://52.166.147.106:8080/"
 
 var inReport = false
 
