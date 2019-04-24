a5f7b9632d9de3f355bfcc1c14eaef2ed1de0cb3
Anthony Van Noppen <anthony.vannoppen@reamdolmen.com>
2019-04-24 13:53:24 +0200

    xml damage fragments

--- app/src/main/java/com/example/europeesaanrijdingsformulier/report/ReportDamageAFragment.kt
+++ app/src/main/java/com/example/europeesaanrijdingsformulier/report/ReportDamageAFragment.kt
@@ -1,4 +1,24 @@
 package com.example.europeesaanrijdingsformulier.report
 
-class ReportDamageAFragment {
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.europeesaanrijdingsformulier.R
+
+
+class ReportDamageAFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_report_damage_a, container, false)
+    }
+
+
 }
