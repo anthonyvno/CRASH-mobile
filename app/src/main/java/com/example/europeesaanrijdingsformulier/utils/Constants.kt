76e773ff9410d019fbd01d94131a53e116fc6377
Anthony Van Noppen <anthony.vannoppen@reamdolmen.com>
2019-05-17 14:04:06 +0200

    online

--- app/src/main/java/com/example/europeesaanrijdingsformulier/utils/Constants.kt
+++ app/src/main/java/com/example/europeesaanrijdingsformulier/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.europeesaanrijdingsformulier.utils
 
-//const val BASE_URL: String = "http://52.166.147.106:8080/"
-const val BASE_URL: String = "http://10.0.2.2:8080/"
+const val BASE_URL: String = "http://52.166.147.106:8080/"
+//const val BASE_URL: String = "http://10.0.2.2:8080/"
 var inReport = false
 val dateFormatValue = "EEE, dd MMM yyyy HH:mm:ss"
 
